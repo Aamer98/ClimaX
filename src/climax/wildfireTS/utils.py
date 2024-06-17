5cdfb82f2fa5105adcdeac9cba679c978ade5e60
aamer98 <ar.aamer@gmail.com>
2024-06-17 15:32:02 +0000

    early stopping

--- src/climax/wildfireTS/utils.py
+++ src/climax/wildfireTS/utils.py
@@ -19,7 +19,7 @@ def wandb_setup(cli) -> None:
 
     wandb.init(
         # Set the project where this run will be logged
-        project="ClimaX_wildfireTS", 
+        project="ClimaX_wildfireTS_avgprec", 
         # We pass a run name (otherwise it’ll be randomly assigned, like sunshine-lollypop-10)
         # Track hyperparameters and run metadata
         config={
